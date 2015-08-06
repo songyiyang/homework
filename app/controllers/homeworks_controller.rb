d6bc192494dc3c08bffaf7ce0b2253925e4e3d31
songyiyang <songyiyang2012@gmail.com>
2015-08-05 21:40:34 -0400

    add homeowrk controller

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -1,5 +1,7 @@
 class HomeworksController < ApplicationController
   def index
   	@homework = Homework.find(params[:id])
-  end
+	rescue ActiveRecord::RecordNotFound
+	  render(:file => "#{Rails.root}/public/404", :layout => 'application', :status => :not_found)
+	end
 end
